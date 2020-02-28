74be195b17af41ea6388d4b44f4a215c66168190
ebisLab <ebaiden@techie.com>
2020-02-28 13:01:40 -0500

    logs in and pushes successfully

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
-const Login = () => {
+const Login = (props) => {
   const [login, setLogin] = useState({
     username: '',
     password: ''
@@ -8,17 +9,34 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   
+  
   const changeHandler =(e)=>{
     setLogin({...login, [e.target.name]: e.target.value})
     console.log(login, '<--login')
     console.log(e.target.value)
+  }
+
+  const submitHandler = e =>{
+    e.preventDefault();
+    axiosWithAuth()
+    .post(`/login`, login)
+    .then(res=>{
+      console.log(res, 'res')
+//       // setLogin(res.data)
+      window.localStorage.setItem('token', res.data.payload)
+props.history.push(`/pallette`)
+    })
+    .catch(err=>err)
+    
+console.log('HELLO WORLD')
+
   }
   
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
-      <form>
+      <form onSubmit={submitHandler}>
         <input
         placeholder="username"
         label="username"
